4fdce6665ab7fa0557d88f388b434c2dbb476c6e
vidhibaldawa <86547950+vidhibaldawa@users.noreply.github.com>
2022-09-13 15:32:18 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,8 +4,6 @@ provider "aws" {
    
 }
 
-# Create VPC
-# terraform aws create vpc
 resource "aws_vpc" "vpc" {
   cidr_block              = var.vpc-cidr
   instance_tenancy        = "default"
@@ -16,8 +14,6 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Create Internet Gateway and Attach it to VPC
-# terraform aws create internet gateway
 resource "aws_internet_gateway" "internet-gateway" {
   vpc_id    = aws_vpc.vpc.id
 
@@ -26,8 +22,7 @@ resource "aws_internet_gateway" "internet-gateway" {
   }
 }
 
-# Create Public Subnet 1
-# terraform aws create subnet
+
 resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public-subnet-1-cidr
@@ -39,8 +34,6 @@ resource "aws_subnet" "public-subnet-1" {
   }
 }
 
-# Create Public Subnet 2
-# terraform aws create subnet
 resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public-subnet-2-cidr
@@ -52,8 +45,7 @@ resource "aws_subnet" "public-subnet-2" {
   }
 }
 
-# Create Route Table and Add Public Route
-# terraform aws create route table
+
 resource "aws_route_table" "public-route-table" {
   vpc_id       = aws_vpc.vpc.id
 
@@ -67,22 +59,19 @@ resource "aws_route_table" "public-route-table" {
   }
 }
 
-# Associate Public Subnet 1 to "Public Route Table"
-# terraform aws associate subnet with route table
+
 resource "aws_route_table_association" "public-subnet-1-route-table-association" {
   subnet_id           = aws_subnet.public-subnet-1.id
   route_table_id      = aws_route_table.public-route-table.id
 }
 
-# Associate Public Subnet 2 to "Public Route Table"
-# terraform aws associate subnet with route table
+
 resource "aws_route_table_association" "public-subnet-2-route-table-association" {
   subnet_id           = aws_subnet.public-subnet-2.id
   route_table_id      = aws_route_table.public-route-table.id
 }
 
-# Create Private Subnet 1
-# terraform aws create subnet
+
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = var.private-subnet-1-cidr
@@ -94,8 +83,7 @@ resource "aws_subnet" "private-subnet-1" {
   }
 }
 
-# Create Private Subnet 2
-# terraform aws create subnet
+
 resource "aws_subnet" "private-subnet-2" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = var.private-subnet-2-cidr
@@ -107,8 +95,7 @@ resource "aws_subnet" "private-subnet-2" {
   }
 }
 
-# Create Private Subnet 3
-# terraform aws create subnet
+
 resource "aws_subnet" "private-subnet-3" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = var.private-subnet-3-cidr
@@ -120,8 +107,6 @@ resource "aws_subnet" "private-subnet-3" {
   }
 }
 
-# Create Private Subnet 4
-# terraform aws create subnet
 resource "aws_subnet" "private-subnet-4" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = var.private-subnet-4-cidr
